bcc520259be4e7981be3a9f33c0d613d83a00726
jelenajjo <jelena.jjo@gmail.com>
2017-03-12 16:46:54 +0100

    responsive

--- src/components/About/AppDev.js
+++ src/components/About/AppDev.js
@@ -5,13 +5,13 @@ class AppDev extends React.Component {
     return (
       <div className="aboutAppDev">
         <div className="container">
-          <div className="col-md-6">
+          <div className="col-md-6 aboutAppDev-txt">
             <h2 className="enterLeft-dev"><strong>App Development</strong></h2>
             <p className="enterLeft-dev">Vanila crew is in love with Meteor.js, which is latest technology based <br></br>on JavaScript (Node.js).</p>
              <br></br>
             <p className="enterLeft-dev">It gives you fast, realtime, scalable applications which work on all <br></br>modern browsers and devices.</p>
           </div>
-          <div className="col-md-6"><img className="enterRight-dev" src={require('../../../public/img/homeAssets/app-dev.png')} /></div>
+          <div className="col-md-6 aboutAppDev-img"><img className="enterRight-dev" src={require('../../../public/img/homeAssets/app-dev.png')} /></div>
         </div>
         </div>
     );
