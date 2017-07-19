f7c9bb4779cf1aede3e68abc637fe023334f3431
jelenajjo <jelena.jjo@gmail.com>
2017-07-19 18:45:17 +0200

    go back

--- src/views/HelmetTest.js
+++ src/views/HelmetTest.js
@@ -5,17 +5,7 @@ class HelmetTest extends React.Component {
   render() {
     return (
       <div style={{height: '200px'}}>
-        <Helmet
-          title="my-title"
-          meta={[
-            { name: 'description', content: 'super-uber application' }
-          ]} />
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      
       </div>
     );
   }
