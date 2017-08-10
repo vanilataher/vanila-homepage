5116de280fa4e77fd029e56a4580fb9e22c80313
jelenajjo <jelena.jjo@gmail.com>
2017-08-10 17:28:21 +0200

    remove lab link

--- src/components/Header/Navigation.js
+++ src/components/Header/Navigation.js
@@ -33,7 +33,6 @@ class Navigation extends React.Component {
 
           <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
             <ul className="nav navbar-nav navbar-right">
-              <li><a href="https://lab.vanila.io/" target="_blank" onClick={this.onClickPlay}>Lab</a></li>
               <li><a href="https://links.vanila.io/" target="_blank" onClick={this.onClickPlay}>Community links</a></li>
               <li><a href="https://blog.vanila.io/" target="_blank" onClick={this.onClickPlay}>Blog</a></li>
               <li><a href="#contact" className="aContact" onClick={this.onClickPlay}><strong>Start Project</strong></a></li>
