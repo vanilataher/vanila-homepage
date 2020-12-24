32f79b1474ad9704d83d6318a27ca3e4127756aa
Md Abu Taher <entrptaher@gmail.com>
2020-12-24 21:59:35 +0600

    fix: remove self-hosted fonts

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Router, browserHistory} from 'react-router';
+import { Router, browserHistory } from 'react-router';
 import routes from './config/routes';
 
 // Css Files
 import './styles/home.css';
 
-class App extends React.Component {
-    render() {
-        return (
-            <Router history={browserHistory}>{routes}</Router>
-        );
-    }
+// https://stackoverflow.com/a/5800856
+function autoAlt() {
+  for (let i = 0; i < document.getElementsByTagName('img').length; i++) {
+    document
+      .getElementsByTagName('img')
+      [i].setAttribute('alt', document.getElementsByTagName('img')[i].src);
+  }
 }
-ReactDOM.render(<App/>, document.getElementById('root'));
+
+class App extends React.Component {
+  componentDidMount() {
+    autoAlt();
+  }
+  render() {
+    return <Router history={browserHistory}>{routes}</Router>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById('root'));
