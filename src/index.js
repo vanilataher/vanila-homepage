727e4e1d82f759bcf2f3704b030628dd93e8dbda
Md Abu Taher <entrptaher@gmail.com>
2020-12-24 19:45:49 +0600

    feat: use cdn and less modules

--- src/index.js
+++ src/index.js
@@ -4,10 +4,7 @@ import {Router, browserHistory} from 'react-router';
 import routes from './config/routes';
 
 // Css Files
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'font-awesome/css/font-awesome.css';
 import './styles/home.css';
-
 
 class App extends React.Component {
     render() {
